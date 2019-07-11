5728e01acba33744d5b02ff14cf8f15d873de303
Andrew Howard <andrew.r.howard@accenture.com>
2019-07-11 13:10:54 -0400

    Migrating to SDL and Box2d. WIP.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
-#include "Polycode.h"
-#include "PolycodeView.h"
-#include "BlockTowerApp.h"
+#include <iostream>
+#include <SDL2/SDL.h>
 
 int main(int argc, char *argv[]) {
 
-	//Create window for the app to utilize. 
-	PolycodeView *view = new PolycodeView("Block Tower GA");
-
-	//Create new instance of app, using the created PolycodeView.  
-	BlockTowerApp *app = new BlockTowerApp(view);
-
-	
-
-
-	//Build the tower, based upon the genome.  
-	//app->buildTower(g);
-
-	//Run the app until exit. 
-	while(app->update()) {}
-
+	std::cout<<"Hello World";
+	SDL_Init(SDL_INIT_VIDEO);
 	return 0;
 }
