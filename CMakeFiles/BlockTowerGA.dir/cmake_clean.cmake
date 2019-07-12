30d2db3a4c3e24b2b9da7ed32ea4cae7dca0ea35
aborthonormal <andrew.richard.howard@gmail.com>
2019-07-12 15:39:48 -0400

    Using cmake.  Using SDL library installed on machine, not including copy in repo.  Created git submodule to link to Box2D project.

---
+++ CMakeFiles/BlockTowerGA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BlockTowerGA.dir/src/main.cpp.o"
+  "BlockTowerGA.pdb"
+  "BlockTowerGA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlockTowerGA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-
-int main(int argc, char *argv[]) {
-
-	std::cout<<"Hello World";
-	SDL_Init(SDL_INIT_VIDEO);
-	return 0;
-}
