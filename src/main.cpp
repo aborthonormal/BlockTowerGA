38ae8931c9d70c5cd8a984ba8ccea1ff758769c3
aborthonormal <andrew.richard.howard@gmail.com>
2019-07-18 17:03:01 -0400

    Box2D and SDL now work with demo code.  Had to use Box2D as external library, not by compiling from git submodule.  Removed tracking for extraneous cmake files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,161 @@
 #include <iostream>
-#include <Box2D/Box2D.h>
 
+#include <Box2D/Box2D.h>
 #include "SDL.h"
 
 #define PTM_RATIO 32.0
 
-int main() {
+int main()
+{
 
-	SDL_Window *window;                    // Declare a pointer
+    SDL_Window *window; //display window
+    SDL_Renderer *renderer; //window renderer
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    SDL_Init(SDL_INIT_VIDEO); // Initialize SDL2
 
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
-        "BlockTowerGA",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
+        "BlockTowerGA",          // window title
+        SDL_WINDOWPOS_UNDEFINED, // initial x position
+        SDL_WINDOWPOS_UNDEFINED, // initial y position
+        800,                     // width, in pixels
+        600,                     // height, in pixels
+        SDL_WINDOW_OPENGL        // flags - see below
     );
 
     // Check that the window was successfully created
-    if (window == NULL) {
+    if (window == NULL)
+    {
         // In the case that the window could not be made...
         printf("Could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
-	// A basic main loop to prevent blocking
-	bool is_running = true;
-	SDL_Event event;
-	while (is_running) {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				is_running = false;
-			}
-		}
-		SDL_Delay(16);
-	}
+    //initialize renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-    // Clean up
-    SDL_Quit();
-
-	// sf::RenderWindow game(sf::VideoMode(800, 600, 32), "game");
+    // sf::RenderWindow game(sf::VideoMode(800, 600, 32), "game");
     // sf::Shape box = sf::Shape::Rectangle(0, 0, 10, 10, sf::Color(255,0,0));
     // sf::Shape ground = sf::Shape::Rectangle(0,540,800,560,sf::Color(0,0,255));
 
+    SDL_Rect box = {0, 0, 10, 10};
+    SDL_Rect ground = {0, 540, 800, 560};
 
+    b2Vec2 gravity(0.0f, 9.8f);
 
-//     b2Vec2 gravity(0.0f, 9.8f);
+    bool doSleep = true;
 
-//     bool doSleep = true;
+    b2World *world;
+    world = new b2World(gravity);
+    world->SetGravity(gravity);
 
+    
+    b2Body *groundBody;
+    b2BodyDef groundBodyDef;
+    groundBodyDef.type = b2_staticBody;
+    groundBodyDef.position.Set(0, 540);
+    groundBody = world->CreateBody(&groundBodyDef);
 
+    b2Body *Body;
+    b2BodyDef ballBodyDef;
+    ballBodyDef.type = b2_dynamicBody;
+    b2Vec2 ballvector;
+    ballvector.Set(10, 10);
+    ballBodyDef.angularVelocity = 0.0f;
+    ballBodyDef.linearVelocity = ballvector;
 
-//     b2World*  world;
-//     world = new b2World(gravity,doSleep);
-//     world -> SetGravity(gravity);
-//     b2Body *groundBody;
-//     b2BodyDef groundBodyDef;
-//     groundBodyDef.type = b2_staticBody;
-//     groundBodyDef.position.Set(0,540);
-//     groundBody = world->CreateBody(&groundBodyDef);
+    ballBodyDef.position.Set(0, 0);
+    ballBodyDef.awake = true;
+    Body = world->CreateBody(&ballBodyDef);
 
-//     b2Body* Body ;
-//     b2BodyDef ballBodyDef;
-//     ballBodyDef.type = b2_dynamicBody;
-//     b2Vec2 ballvector;
-//     ballvector.Set(10,10);
-//     ballBodyDef.angularVelocity = 0.0f;
-//     ballBodyDef.linearVelocity = ballvector;
+    b2EdgeShape groundEdge;
+    groundEdge.Set( b2Vec2(0,0), b2Vec2(800,0) );
+    b2FixtureDef boxShapeDef;
+    boxShapeDef.shape = &groundEdge;
+    boxShapeDef.density = 2.0f;
+    boxShapeDef.restitution = 0.5f;
 
-//     ballBodyDef.position.Set(0, 0);
-//     ballBodyDef.awake = true;
-//     Body = world->CreateBody(&ballBodyDef);
+    groundBody->CreateFixture(&groundEdge, 0);
 
+    b2PolygonShape dynamicBox;
 
+    dynamicBox.SetAsBox(10.0f, 10.0f);
 
+    b2FixtureDef fixtureDef;
 
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 2.0f;
+    fixtureDef.friction = 1.0f;
+    fixtureDef.restitution = 0.5f;
 
-//     b2PolygonShape groundBox;
-//     b2FixtureDef boxShapeDef;
-//     boxShapeDef.shape = &groundBox;
-//     boxShapeDef.density = 2.0f;
-//     boxShapeDef.restitution = 0.5f;
-//     groundBox.SetAsEdge( b2Vec2(0,0), b2Vec2(800,0) );
+    b2MassData data;
+    data.mass = 10;
+    Body->SetMassData(&data);
+    Body->CreateFixture(&fixtureDef);
 
-//     groundBody->CreateFixture(&groundBox,0);
+    float timeStep = 1.0f / 300.0f;
 
+    int velIter = 1.0;
+    int posIter = 1.0;
+    world->Step(timeStep, velIter, posIter);
 
+    b2Vec2 pos = Body->GetPosition();
+    float an = Body->GetAngle();
 
+    //set box position and angle
+    // box.SetPosition(pos.x, pos.y);
+    // box.SetRotation(an);
 
-//     b2PolygonShape dynamicBox;
+    box.x = pos.x;
+    box.y = pos.y;
 
-//     dynamicBox.SetAsBox(10.0f, 10.0f);
+    // A basic main loop to prevent blocking
+    bool is_running = true;
+    SDL_Event event;
+    while (is_running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                is_running = false;
+            }
+        }
 
-//     b2FixtureDef fixtureDef;
+        world->Step(timeStep, velIter, posIter);
 
-//     fixtureDef.shape = &dynamicBox;
+        b2Vec2 pos = Body->GetPosition();
+        float angle = Body->GetAngle();
 
-//     fixtureDef.density = 2.0f;
+        // box.SetPosition(pos.x, pos.y);
+        // box.SetRotation(angle);
+        box.x = pos.x;
+        box.y = pos.y;
 
-//     fixtureDef.friction = 1.0f;
+        //Clear the window
+        SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear(renderer);
 
+        //draw box
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF );
+        SDL_RenderFillRect(renderer, &box);
 
-//     fixtureDef.restitution = 0.5f;
+        //draw ground
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF );
+        SDL_RenderFillRect(renderer, &ground);
 
-//     /*b2MassData data;
-//     data.mass = 10;
-//     Body->SetMassData(&data);*/
-//     Body->CreateFixture(&fixtureDef);
+        //update screen
+        SDL_RenderPresent(renderer);
 
-//     float timeStep = 1.0f / 300.0f;
+        SDL_Delay(16);
+    }
 
-
-//     int velIter = 1.0;
-//     int posIter = 1.0;
-//     world->Step(timeStep, velIter, posIter);
-
-
-//     b2Vec2 pos = Body->GetPosition();
-//     float an = Body->GetAngle();
-
-//     box.SetPosition(pos.x, pos.y);
-//     box.SetRotation(an);
-
-
-// bool Running = true;
-//     while (Running)
-//     {
-//         sf::Event Event;
-
-//         while (game.GetEvent(Event))
-//         {
-//            if (Event.Type == sf::Event::Closed)
-//            Running = false;
-//         }
-
-//             world->Step(timeStep, velIter, posIter);
-
-
-
-//             b2Vec2 pos = Body->GetPosition();
-//             float angle = Body->GetAngle();
-
-//             box.SetPosition(pos.x, pos.y);
-//             box.SetRotation(angle);
-
-
-
-//             game.Clear();
-//             game.Draw(ground);
-//             game.Draw(box);
-
-//         game.Display();
-//     }
-
-//     game.Close();
+    //destroy window
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    renderer = NULL;
+    window = NULL;
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
